8ff2fc4660c4a69d6b93f0a5ee64de591f445cab
oksanaisuprun <oksanaisuprun@gmail.com>
2020-01-13 22:22:11 +0200

    Task 4

---
+++ task_4/Palindrome.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_4
+{
+	class Palindrome
+	{
+		public static bool IsPalindrome(string inputData)
+		{
+			int min = 0;
+			int max = inputData.Length - 1;
+			while (true)
+			{
+				if (min > max)
+				{
+					return true;
+				}
+				char stringBeggining = inputData[min];
+				char stringEnding = inputData[max];
+
+				// Scan forward for stringBeggining while invalid.
+				while (!char.IsLetterOrDigit(stringBeggining))
+				{
+					min++;
+					if (min > max)
+					{
+						return true;
+					}
+					stringBeggining = inputData[min];
+				}
+
+				// Scan backward for stringEnding while invalid.
+				while (!char.IsLetterOrDigit(stringEnding))
+				{
+					max--;
+					if (min > max)
+					{
+						return true;
+					}
+					stringEnding = inputData[max];
+				}
+
+				if (char.ToLower(stringBeggining) != char.ToLower(stringEnding))
+				{
+					return false;
+				}
+				min++;
+				max--;
+			}
+		}
+	}
+}
+
